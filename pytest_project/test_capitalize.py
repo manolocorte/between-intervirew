b53784786faa06e2ce108b73c7e0b61490a2ada3
root <root@Alpha-D.localdomain>
2023-03-13 15:24:32 +0100

    Adding Pytest

---
+++ pytest_project/test_capitalize.py
@@ -0,0 +1,12 @@
+# test_capitalize.py
+
+import pytest
+
+def capital_case(x):
+    if not isinstance(x, str):
+        raise TypeError('Please provide a string argument')
+    return x.capitalize()
+
+def test_raises_exception_on_non_string_arguments():
+    with pytest.raises(TypeError):
+        capital_case(9)
